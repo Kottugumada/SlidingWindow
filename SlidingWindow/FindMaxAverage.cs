06695081dd7cd46b61cb71a457d57832655c778f
kottugumada <karthikkottugumada@outlook.com>
2020-01-10 22:08:01 -0800

    Max Avg

---
+++ SlidingWindow/FindMaxAverage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SlidingWindow
+{
+	public class MaxAverage
+	{
+		public double FindMaxAverage(int[] nums, int k)
+		{
+			// set it to a high min
+			double res = double.MinValue;
+			if (nums.Length == 0) return res;
+
+			int windowSum = 0;
+			int windowStart = 0;
+			for (int windowEnd = 0; windowEnd < nums.Length; windowEnd++)
+			{
+				windowSum = windowSum + nums[windowEnd];
+				// stop moving the pointer when you hit the window maxima
+				if (windowEnd >= k - 1)
+				{
+					double avg = windowSum / (double) k;
+					res = Math.Max(avg, res);
+					// progress the window further by removing the head and adding the tail pointer
+					windowSum = windowSum - nums[windowStart];
+					windowStart++;
+				}
+			}
+			return Convert.ToDouble(res);
+		}
+	}
+}
--- SlidingWindow/Program.cs
+++ SlidingWindow/Program.cs
@@ -17,9 +17,14 @@ namespace SlidingWindow
             MaxSumOfContiguousSubArray maxSum = new MaxSumOfContiguousSubArray();
             Console.Write(maxSum.MaxSumOfSubArrayOfSizeK(3,new int[] { 2, 1, 5, 1, 3, 2 }));
 
-            // Window size is not fixed
-            // https://www.educative.io/courses/grokking-the-coding-interview/7XMlMEQPnnQ
-            Console.WriteLine("Given an array of positive numbers and a positive number ‘S’, find the length of the smallest contiguous subarray whose sum is greater than or equal to ‘S’. Return 0, if no such subarray exists.");
+			// https://leetcode.com/problems/maximum-average-subarray-i/
+			Console.WriteLine("Given an array consisting of n integers, find the contiguous subarray of given length k that has the maximum average value. And you need to output the maximum average value.");
+			MaxAverage maxAvg = new MaxAverage();
+			maxAvg.FindMaxAverage(new int[] { 1, 12, -5, -6, 50, 3 }, 4);
+
+			// Window size is not fixed
+			// https://www.educative.io/courses/grokking-the-coding-interview/7XMlMEQPnnQ
+			Console.WriteLine("Given an array of positive numbers and a positive number ‘S’, find the length of the smallest contiguous subarray whose sum is greater than or equal to ‘S’. Return 0, if no such subarray exists.");
             SmallestSubArray sm = new SmallestSubArray();
             Console.Write(sm.MinSizeSubArray(7, new int[] { 2, 1, 5, 2, 3, 2 }));
 
@@ -31,6 +36,6 @@ namespace SlidingWindow
             Anagrams an = new Anagrams();
             string[] strs = new string[] { "eat", "tea", "tan", "ate", "nat", "bat" };
             an.GroupAnagrams(strs);
-        }
+		}
     }
 }
